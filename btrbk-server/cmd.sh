0ae6ce5b51c7833b698460477b2b2c97c9f49048
Fabian Hauck <hauckfabian@gmail.com>
2021-06-22 22:47:53 +0200

    btrbk-server container with none root user

--- btrbk-server/cmd.sh
+++ btrbk-server/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-PW=$(head -c 128 /dev/urandom | base64 | tr -d '\n\r'); echo -n "$PW\n$PW" | passwd root
+PW=$(head -c 128 /dev/urandom | base64 | tr -d '\n\r'); echo -n "$PW\n$PW" | passwd btrbk
 
 /usr/sbin/sshd -eD
